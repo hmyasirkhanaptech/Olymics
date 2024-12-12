0d614e76d14771ec447a6384be2b0dbb85c19380
hmyasirkhanaptech <yasir_khan@aptechnorth.edu.pk>
2024-12-12 20:40:13 +0500

    Add files via upload

---
+++ HTML/script.js
@@ -0,0 +1,30 @@
+function showinput() {
+    var inputsearch =document.querySelector('.s-input')
+    // inputsearch.className = 's-inputhover'
+    inputsearch.style.display = 'block'
+    inputsearch.style.width = '300px'
+    
+       
+
+}
+
+
+function darkmode() {
+    var inputsearch =document.querySelector('body')
+    // inputsearch.className = 's-inputhover'
+    inputsearch.style.backgroundColor = 'black'
+    inputsearch.style.color = 'white'
+    
+       
+
+}
+
+function lightmode() {
+    var inputsearch =document.querySelector('body')
+    // inputsearch.className = 's-inputhover'
+    inputsearch.style.backgroundColor = 'white'
+    inputsearch.style.color = 'black'
+    
+       
+
+}
